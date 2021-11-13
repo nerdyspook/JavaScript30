97c855632258ddfcdcf35913a7234767380c1c1b
Susanto <nerdyspook007@gmail.com>
2021-11-13 15:43:30 +0530

    styles: added comments

--- 03-CSS-Variables/main.js
+++ 03-CSS-Variables/main.js
@@ -1,8 +1,8 @@
 const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
-    const suffix = this.dataset.sizing || "";
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const suffix = this.dataset.sizing || ""; // adds px or nothing at the end
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // returns the Element that is the root element of the document
 }
 
 inputs.forEach(input => {
